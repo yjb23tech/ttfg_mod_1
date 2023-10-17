56dbab5b2d576cbe9cc1571a5cbc967aa644167a
yjb23tech <yjb23tech@gmail.com>
2023-10-17 22:05:40 +0100

    Happy Hunting

--- class_player.py
+++ class_player.py
@@ -1,3 +1,5 @@
+from class_weapon import Weapon, BattleAxe, LongSword 
+
 class Player:
 
     def __init__(self):
@@ -5,8 +7,28 @@ class Player:
         self.str_name = "Yuri Orlov"
         self.str_place_of_birth = "Odessa"
         self.str_msg = "peace is bad for business"
+
+        self.arr_inventory = [BattleAxe(), LongSword()]
+        self.obj_current_weapon = None 
     
     def __str__(self):
 
-        return (f"My name is {self.str_name}; always remember: {self.str_msg}")
+        return (f"\nMy name is {self.str_name}; always remember: {self.str_msg}")
+    
+    def display_inventory(self):
+
+        print("\nIn your inventory you have access to the following weapons:")
+        for x, weapon in enumerate(self.arr_inventory, 1):
+            print(f"{x}. {weapon}")
+    
+    def display_current_weapon(self):
+
+        if self.obj_current_weapon == None:
+            print(f"\n{self.str_name} is currently unarmed - this is dangerous!\n")
+        else:
+            print(f"\n{self.str_name} is currently armed - you are dangerous XD\n")
+    
+
+    
+
 
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from class_player import Player
 from class_weapon import Weapon, BattleAxe, LongSword
 
+#test_weapon = BattleAxe()
+#print(test_weapon)
+
+#test_weapon_2 = LongSword()
+#print(test_weapon_2)
+
 user_player = Player()
 print(user_player)
 
-test_weapon = BattleAxe()
-print(test_weapon)
-
-test_weapon_2 = LongSword()
-print(test_weapon_2)
-
+user_player.display_inventory()
+user_player.display_current_weapon()
